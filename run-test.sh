e1ea7ef4c5ef6ac1d9ba9bb1d1f8cf39945d3cb3
Fabiana Zioti <fabiana_zioti@hotmail.com>
2019-11-26 14:19:44 -0300

    adding template

---
+++ run-test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# This file is part of Web Land Trajectory Service.
+# Copyright (C) 2019 INPE.
+#
+# Land Cover Classification System Database Model is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+pydocstyle lccs_db && \
+isort --check-only --diff --recursive **/*.py && \
+check-manifest --ignore ".travis-*" && \
+pytest
---
+++ tests/test_model.py
@@ -0,0 +1,9 @@
+#
+# This file is part of Web Land Trajectory Service.
+# Copyright (C) 2019 INPE.
+#
+# Land Cover Classification System Database Model is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Unit-test for lccs_db model."""
