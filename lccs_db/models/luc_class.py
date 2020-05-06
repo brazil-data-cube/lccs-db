f9ddd2fb772f1f7b93ebe72f23bc0368ad171a83
Fabiana Zioti <fabi_zioti@hotmail.com>
2020-05-06 13:53:56 -0300

    creating classes view

--- lccs_db/models/luc_class.py
+++ lccs_db/models/luc_class.py
@@ -8,11 +8,13 @@
 
 """Land Cover Classification System Model ."""
 
-from sqlalchemy import Column, ForeignKey, Integer, Text
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, Text, select
+from sqlalchemy.orm import aliased, relationship
+from sqlalchemy_utils import create_view
 
 from ..config import Config
 from .base import BaseModel
+from .luc_classification_system import LucClassificationSystem
 
 
 class LucClass(BaseModel):
@@ -33,3 +35,26 @@ class LucClass(BaseModel):
 
     classification_system = relationship('LucClassificationSystem')
     class_parent = relationship('LucClass')
+
+class ClassesView(BaseModel):
+    __tablename__ = 'v_classes'
+    parent_classes = aliased(LucClass)
+
+    __table__ = create_view(
+        name=__tablename__,
+        selectable=select(
+            [ LucClass.created_at,
+              LucClass.updated_at,
+             LucClass.id,
+             LucClass.name,
+             LucClass.description,
+             LucClass.code,
+             parent_classes.name.label('class_parent_name'),
+             LucClassificationSystem.name.label('class_system_name')],
+            from_obj=(
+                LucClass.__table__.join(LucClassificationSystem, LucClassificationSystem.id == LucClass.class_system_id)
+                    .join(parent_classes,LucClass.class_parent_id == parent_classes.id, isouter=True))
+        ),
+        metadata=BaseModel.metadata,
+    )
+    __table__.schema = Config.LCC_ACTIVE_SCHEMA
