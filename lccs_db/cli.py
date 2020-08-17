61de1127a68b116b048c1b2caa6f43c5bf6226ce
Fabiana Zioti <fabi_zioti@hotmail.com>
2020-08-17 14:51:34 -0300

    Add bdc_db dependency close #51

--- lccs_db/cli.py
+++ lccs_db/cli.py
@@ -7,90 +7,23 @@
 #
 
 """Command line interface for the Land Cover Classification System Database Model ."""
-import os
-import subprocess
-
 import click
-from flask import Flask
-from flask.cli import FlaskGroup, with_appcontext
-from sqlalchemy_utils import create_database, database_exists
+from bdc_db.cli import cli
+from bdc_db.db import db as _db
+from flask.cli import with_appcontext
 
 from lccs_db.data import load_dbdata
-from lccs_db.models import db as _db
-
-from .config import Config as config_infos
-from .ext import LCCSDatabase
 
 
-def create_app():
-    """Create internal flask app."""
-    app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-    LCCSDatabase(app)
-
-    return app
-
-
-def create_cli(create_app=None):
-    """Define a wrapper to create Flask App in order to attach into flask click.
-
-    Args:
-         create_app (function) - Create app factory (Flask).
-    """
-    def create_cli_app(info):
-        if create_app is None:
-            info.create_app = None
-
-            app = info.load_app()
-        else:
-            app = create_app()
-
-        return app
-
-    @click.group(cls=FlaskGroup, create_app=create_cli_app)
-    def cli(*args, **params):
-        pass
-
-    return cli
-
-cli = create_cli(create_app=create_app)
-
-
-@cli.group()
-@with_appcontext
-def db():
-    """Database operations."""
-
-
-@db.command()
-@with_appcontext
-def init_db():
-    """Create database. Make sure the variable SQLALCHEMY_DATABASE_URI is set."""
-    click.secho('Creating database {0}'.format(_db.engine.url),
-                fg='green')
-    if not database_exists(str(_db.engine.url)):
-        create_database(str(_db.engine.url))
-
-    with _db.session.begin_nested():
-        click.secho('Creating schema if not exist {}...'.format(config_infos.LCC_ACTIVE_SCHEMA), fg='green')
-        _db.session.execute("CREATE SCHEMA IF NOT EXISTS {}".format(config_infos.LCC_ACTIVE_SCHEMA))
-
-    _db.session.commit()
-
-@db.command()
+@cli.command()
 @with_appcontext
 @click.option('--ifile', type=click.File('r'),
               help='A SQL input file for insert.',
               required=False)
-def insert_db(ifile):
-    """Insert Data into Database."""
+def insert_lccs_data(ifile):
+    """Insert data in LCCS schema."""
     if ifile is not None:
         sql = ifile.read()
-
     else:
         sql = load_dbdata()
 
@@ -98,4 +31,9 @@ def insert_db(ifile):
 
     _db.session.commit()
 
-    click.echo("Data inserted with success!")
+    click.echo("LCCS Data inserted with success!")
+
+def main(as_module=False):
+    # TODO omit sys.argv once https://github.com/pallets/click/issues/536 is fixed
+    import sys
+    cli.main(args=sys.argv[1:], prog_name="python -m lccs_db" if as_module else None)
