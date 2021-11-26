6d178f111de1d4abc0336154e3d3809ad3fde984
Fabiana Zioti <fabiana_zioti@hotmail.com>
2021-11-26 13:36:47 -0300

    adding decorator for language support

--- lccs_db/utils.py
+++ lccs_db/utils.py
@@ -8,6 +8,32 @@
 
 """Utility for Land Cover Classification System Database Model."""
 import mimetypes
+from functools import wraps
+
+from flask import request
+
+from lccs_db.models.base import translation_hybrid
+
+
+def set_locale(locale: str):
+    """Update the location."""
+    translation_hybrid.current_locale = locale
+
+
+def language(language=None, required=False):
+    """Decorate for locale."""
+    def _language(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            support_language = ['pt-br', 'en']
+            language_str = request.headers['language'] if request.headers.get('language') else \
+                            request.args.get('language')
+            if language_str:
+                if language_str in support_language:
+                    set_locale(language_str)
+            return func(*args, **kwargs)
+        return wrapped
+    return
 
 
 def get_mimetype(style_file):
