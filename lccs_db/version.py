397c3965a70ed97482f8f23247a44186d8058eef
Fabiana Zioti <fabiana_zioti@hotmail.com>
2023-12-01 15:08:55 -0300

    :zap: updating version

--- lccs_db/version.py
+++ lccs_db/version.py
@@ -1,6 +1,6 @@
 #
 # This file is part of LCCS-DB.
-# Copyright (C) 2022 INPE.
+# Copyright (C) 2023 INPE.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,4 +23,4 @@ and parsed by ``setup.py``.
 """
 
 
-__version__ = '0.8.1'
+__version__ = '0.8.2'
